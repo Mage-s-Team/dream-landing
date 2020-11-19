97d6e5520778b5e1f6e9fa319d198bd12948d6e7
HappyKiller <007kostiv@gmail.com>
2020-11-19 20:58:01 +0200

    Add working preloader

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
-const body = document.getElementsByTagName(body);
+const body = document.querySelector('body');
 const preloader = document.querySelector('.preloader-wrap');
+
+function hidePreloader() {
+  body.classList.remove('scrollbar-hide');
+  body.classList.add('scrollbar-unhide');
+  preloader.classList.add('preloader-wrap-hidden');
+}
+
+setInterval(hidePreloader, 5000);
